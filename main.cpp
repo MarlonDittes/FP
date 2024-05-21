9c6f76cbf2ccac459dfb60201a26ed31fb9e7a62
Shaip161 <dorian.peretz@stud.uni-heidelberg.de>
2024-05-21 14:34:33 +0200

    merge with henning should work TODO: add it to the branch and reduce

--- main.cpp
+++ main.cpp
@@ -36,21 +36,21 @@ int main(int argc, char* argv[]) {
     bool almost = (boolArgInt != 0);*/
 
     //Random graph
-    while (true) {
-    int sizeX = 5;
-    int sizeY = 5;
-    std::string output = "../src_henning/data/test/faulty.gr";
-    auto edges = generateBipartiteGraph(sizeX, sizeY);
-    writeGraphToBipartiteGraph(sizeX, sizeY, edges.size(), edges, output);
+    //while (true) {
+        int sizeX = 5;
+        int sizeY = 5;
+        std::string output = "../src_henning/data/test/faulty.gr";
+        auto edges = generateBipartiteGraph(sizeX, sizeY);
+        writeGraphToBipartiteGraph(sizeX, sizeY, edges.size(), edges, output);
 
-    Graph* g = readGraph(graph_file);
-    Graph verifier = *g;
+        Graph* g = readGraph(graph_file);
+        Graph verifier = *g;
 
-    bool check = ExactSolution(*g);
-    if (!check) {
-        return 0;
-    }
-    }
+        bool check = ExactSolution(*g);
+        if (!check) {
+            return 0;
+        }
+    //}
 
     //g->printGraph();
     //std::cout << std::endl;
