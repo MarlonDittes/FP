bd7fb4ef04913fb6dd44929482e605f44806646e
MarlonDittes <mali.dittes@gmail.com>
2024-06-08 21:37:16 +0200

    Removed Output after while(true) which is never reached

--- main.cpp
+++ main.cpp
@@ -90,7 +90,6 @@ int main(int argc, char* argv[]) {
         }
     }
     
-    outputStandardOut(bestSolution);
     //std::cout << bestCrossings << std::endl;
     //outputStandardOut(bestSolution);
 }
