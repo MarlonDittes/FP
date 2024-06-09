bff87d024a9404a102c206abb9597880d36036a4
MarlonDittes <mali.dittes@gmail.com>
2024-06-09 15:59:46 +0200

    CHANGE: Final changes to main structure (still printing crossings for now instead of solution)

--- main.cpp
+++ main.cpp
@@ -36,21 +36,6 @@ int main(int argc, char* argv[]) {
     bestSolution = g->getOrderNodes();
     bestCrossings = g->countCrossings();
 
-    //std::cout << bestCrossings << ",";
-    
-    // Which reductions to use
-    std::vector<std::unique_ptr<general_reduction>> reductions;
-    reductions.push_back(std::make_unique<ZeroEdge_reduction>());
-    reductions.push_back(std::make_unique<Complete_reduction>());
-    //reductions.push_back(std::make_unique<ZeroCrossings_reduction>());
-    reductions.push_back(std::make_unique<Twins_reduction>());
-    //reductions.push_back(std::make_unique<AlmostTwin_reduction>());
-    //reductions.push_back(std::make_unique<Domination_reduction>());
-
-    // Output solution
-    int method2 = 2;
-    bool fast = 1;
-
     TomAlvAlg(*g);
     int currentCrossings = g->countCrossings();
     if (currentCrossings < bestCrossings){
@@ -58,17 +43,35 @@ int main(int argc, char* argv[]) {
         bestCrossings = currentCrossings;
     }
 
-    //std::cout << g->countCrossings() << ",";
-    int exactSize = 10;
-    auto result = BranchAndReduce(g, reductions, 2, method2, fast, exactSize);
+    // Which reductions to use
+    std::vector<std::unique_ptr<general_reduction>> reductions;
+    reductions.push_back(std::make_unique<ZeroEdge_reduction>());
+    reductions.push_back(std::make_unique<Complete_reduction>());
+    //reductions.push_back(std::make_unique<ZeroCrossings_reduction>());
+    reductions.push_back(std::make_unique<Twins_reduction>());
+    //reductions.push_back(std::make_unique<Domination_reduction>());
+
+    // Output solution
+    int method2 = 2;
+    bool fast = 1;
+
+    auto result = BranchAndReduce(g, reductions, 2, method2, fast, 0);
     currentCrossings = g->countCrossings();
     if (currentCrossings < bestCrossings){
         bestSolution = result.first;
         bestCrossings = currentCrossings;
     }
 
+    int exactSize = 10;
+    result = BranchAndReduce(g, reductions, 2, method2, fast, exactSize);
+    currentCrossings = g->countCrossings();
+    if (currentCrossings < bestCrossings){
+        bestSolution = result.first;
+        bestCrossings = currentCrossings;
+    }
     
     while(true){
+        exactSize += 2;
         g->MedianHeuristic();
         result = BranchAndReduce(g, reductions, 3, method2, fast, exactSize);
         currentCrossings = g->countCrossings();
@@ -76,7 +79,6 @@ int main(int argc, char* argv[]) {
             bestSolution = result.first;
             bestCrossings = currentCrossings;
         }
-        exactSize += 5;
     }
     
     //term(0);
